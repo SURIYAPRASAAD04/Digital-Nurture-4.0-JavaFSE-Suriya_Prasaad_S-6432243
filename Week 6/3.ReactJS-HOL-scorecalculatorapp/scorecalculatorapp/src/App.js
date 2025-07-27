2e8223a9f1d65c3d41f8676bde0661849341415d
Suriya Prasaad S <158862094+SURIYAPRASAAD04@users.noreply.github.com>
2025-07-27 10:43:33 +0530

    Week 6

---
+++ Week 6/3.ReactJS-HOL-scorecalculatorapp/scorecalculatorapp/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import { CalculateScore } from '../src/Components/CalculateScore';
+
+function App() {
+  return (
+    <div>
+      <CalculateScore
+        Name="Suriya Prasaad S"
+        School="Sri Manakula Vinayagar Engineering College"
+        total={489}
+        goal={500}
+        SuperSetID="6432243"
+      />
+    </div>
+  );
+}
+
+export default App;
